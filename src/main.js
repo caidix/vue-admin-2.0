773ee31286a2c953a7a4ceaada69bdeff410346a
CD <48509493+caidix@users.noreply.github.com>
2020-04-24 00:21:35 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
+import Vue from "vue";
+import App from "./App.vue";
+import "normalize.css";
+import ElementUI from "element-ui";
+import router from "./router";
+import store from "./store";
+Vue.config.productionTip = false;
+import "element-ui/lib/theme-chalk/index.css";
+Vue.use(ElementUI);
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  store,
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
